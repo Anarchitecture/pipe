72e0f119be58956de47ef7e0cac5642b7ca6d748
Laurens Hoek <lrhoek@gmail.com>
2026-01-01 16:06:26 +0100

    Consistency in naming, add missing parameters

--- src/pipe.php
+++ src/pipe.php
@@ -7,18 +7,18 @@ namespace Anarchitecture\pipe;
 /**
  * Return unary callable for array_any
  */
-function array_any(callable $c) : callable {
-    return function (array $a) use ($c) : bool {
-        return \array_any($a, $c);
+function array_any(callable $callback) : callable {
+    return function (array $array) use ($callback) : bool {
+        return \array_any($array, $callback);
     };
 }
 
 /**
  * Return unary callable for array_all
  */
-function array_all(callable $c) : callable {
-    return function (array $a) use ($c) : bool {
-        return \array_all($a, $c);
+function array_all(callable $callback) : callable {
+    return function (array $array) use ($callback) : bool {
+        return \array_all($array, $callback);
     };
 }
 
@@ -26,35 +26,35 @@ function array_all(callable $c) : callable {
  * Return unary callable for array_chunk
  */
 function array_chunk(int $length, bool $preserve_keys = false) : callable {
-    return function (array $a) use ($length, $preserve_keys) : array {
-        return \array_chunk($a, $length, $preserve_keys);
+    return function (array $array) use ($length, $preserve_keys) : array {
+        return \array_chunk($array, $length, $preserve_keys);
     };
 }
 
 /**
  * Return unary callable for array_filter
  */
-function array_filter(?callable $c = null, int $m = 0) : callable {
-    return function (array $a) use ($c, $m) : array {
-        return \array_filter($a, $c, $m);
+function array_filter(?callable $callback = null, int $mode = 0) : callable {
+    return function (array $array) use ($callback, $mode) : array {
+        return \array_filter($array, $callback, $mode);
     };
 }
 
 /**
  * Return unary callable for array_map
  */
-function array_map(callable $c) : callable {
-    return function (array $a) use ($c) : array {
-        return \array_map($c, $a);
+function array_map(callable $callback) : callable {
+    return function (array $array) use ($callback) : array {
+        return \array_map($callback, $array);
     };
 }
 
 /**
  * Return unary callable for array_reduce
  */
-function array_reduce(callable $c, mixed $init = null) : callable {
-    return function (array $a) use ($c, $init) : mixed {
-        return \array_reduce($a, $c, $init);
+function array_reduce(callable $callback, mixed $initial = null) : callable {
+    return function (array $array) use ($callback, $initial) : mixed {
+        return \array_reduce($array, $callback, $initial);
     };
 }
 
@@ -62,42 +62,46 @@ function array_reduce(callable $c, mixed $init = null) : callable {
  * Return unary callable for array_slice
  */
 function array_slice(int $offset, ?int $length = null, bool $preserve_keys = false) : callable {
-    return function (array $a) use ($offset, $length, $preserve_keys) : array {
-        return \array_slice($a, $offset, $length, $preserve_keys);
+    return function (array $array) use ($offset, $length, $preserve_keys) : array {
+        return \array_slice($array, $offset, $length, $preserve_keys);
     };
 }
 
 /**
  * Return unary callable for explode
  */
-function explode(string $d) : callable {
-    return function (string $s) use ($d) : array {
-        return \explode($d, $s);
+function explode(string $separator, int $limit = PHP_INT_MAX) : callable {
+    return function (string $string) use ($separator, $limit) : false|array {
+        return \explode($separator, $string, $limit);
     };
 }
 
 /**
- * By-reference cleaning wrapper for rsort
+ * Return unary callable for rsort
  */
-function rsort(array $a) : array {
-    \rsort($a);
-    return $a;
+function rsort(int $flags = SORT_REGULAR) : callable {
+    return function (array $array) use ($flags) : array {
+        \rsort($array, $flags);
+        return $array;
+    };
 }
 
 /**
- * By-reference cleaning wrapper for sort
+ * Return unary callable for rsort
  */
-function sort(array $a) : array {
-    \sort($a);
-    return $a;
+function sort(int $flags = SORT_REGULAR) : callable {
+    return function (array $array) use ($flags) : array {
+        \sort($array, $flags);
+        return $array;
+    };
 }
 
 /**
  * Return unary callable for usort
  */
-function usort(callable $c) : callable {
-    return function (array $a) use ($c) : array {
-        \usort($a, $c);
-        return $a;
+function usort(callable $callback) : callable {
+    return function (array $array) use ($callback) : array {
+        \usort($array, $callback);
+        return $array;
     };
 }
