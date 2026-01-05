1532940ef79c4d8c6fbac7763a5aa927186720f1
Laurens Hoek <lrhoek@gmail.com>
2026-01-05 10:34:33 +0100

    add iterable_map

--- src/pipe.php
+++ src/pipe.php
@@ -217,6 +217,20 @@ function iterable_first(iterable $iterable): mixed {
 }
 
 /**
+ * Return unary callable for mapping over an iterable
+ *
+ * @param callable(mixed, mixed) : mixed $callback
+ * @return Closure(iterable<array-key, mixed>) : Generator
+ */
+function iterable_map(callable $callback) : Closure {
+    return function (iterable $iterable) use ($callback) : Generator {
+        foreach ($iterable as $key => $value) {
+            yield $key => $callback($value, $key);
+        }
+    };
+}
+
+/**
  * Return unary callable for taking $count items from an iterable
  *
  * @param int<0, max> $count
