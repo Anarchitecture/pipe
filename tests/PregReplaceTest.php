fcd269bba977f46848acbc1a693d167063e9520d
Laurens Hoek <lrhoek@gmail.com>
2026-01-18 16:52:34 +0100

    add preg_replace test

---
+++ tests/PregReplaceTest.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Anarchitecture\pipe\Tests;
+
+use Closure;
+use PHPUnit\Framework\TestCase;
+
+use function Anarchitecture\pipe\preg_replace;
+
+final class PregReplaceTest extends TestCase {
+
+    public function test_returns_a_closure() : void {
+
+        $stage = preg_replace('/x/', 'y');
+
+        self::assertInstanceOf(Closure::class, $stage);
+    }
+
+    public function test_replaces_in_a_string_subject() : void {
+
+        $stage = 'axbxc';
+
+        $result = $stage
+            |> preg_replace('/x/', 'Z');
+
+        self::assertSame('aZbZc', $result);
+    }
+
+    public function test_respects_limit() : void {
+
+        $stage = 'axbxcxd';
+
+        $result = $stage
+            |> preg_replace('/x/', 'Z', 2);
+
+        self::assertSame('aZbZcxd', $result);
+    }
+
+    public function test_replaces_in_an_array_subject() : void {
+
+        $stage = ['a1', 'b2', 'c3'];
+
+        $result = $stage
+            |> preg_replace('/\d/', 'X');
+
+        self::assertSame(['aX', 'bX', 'cX'], $result);
+    }
+
+    public function test_supports_array_patterns_and_replacements() : void {
+
+        $stage = 'foo 123 bar';
+
+        $result = $stage
+            |> preg_replace(
+                ['/\bfoo\b/', '/\d+/'],
+                ['qux', 'N']
+            );
+
+        self::assertSame('qux N bar', $result);
+    }
+
+    public function test_returns_null_on_invalid_pattern() : void {
+
+        $stage = 'abc';
+
+        $result = $stage
+            |> preg_replace('/(unclosed/', 'x');
+
+        self::assertNull($result);
+    }
+
+}
